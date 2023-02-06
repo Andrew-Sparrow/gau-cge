b6669c69bc9f2c4f6625fc8c284a28d20546c9b8
Andrew <8911102@gmail.com>
2023-02-06 13:48:26 +0500

    adds sections and statements files for db

---
+++ src/db/sections.js
@@ -0,0 +1,62 @@
+const sections = {
+  sections: [
+    {
+      section: {
+        id: 1,
+        name: 'АР - Архитектурные решения',
+        undreadCount: 0
+      }
+    },
+    {
+      section: {
+        id: 2,
+        name: 'СМ - Сметы',
+        undreadCount: 0
+      }
+    },
+    {
+      section: {
+        id: 3,
+        name: 'КР - Конструктивные решения',
+        undreadCount: 0
+      }
+    },
+    {
+      section: {
+        id: 4,
+        name: 'АР - Архитектурные решения',
+        undreadCount: 0
+      }
+    },
+    {
+      section: {
+        id: 5,
+        name: 'ТП - Технический паспорт БТИ',
+        undreadCount: 0
+      }
+    },
+    {
+      section: {
+        id: 6,
+        name: 'КП - Кадастровый паспорт на земельный участок',
+        undreadCount: 0
+      }
+    },
+    {
+      section: {
+        id: 7,
+        name: 'ПД - Правоустанавливающие и право подтверждающие документы',
+        undreadCount: 0
+      }
+    },
+    {
+      section: {
+        id: 8,
+        name: 'ПП - Поэтажные планы помещений',
+        undreadCount: 0
+      }
+    }
+  ]
+};
+
+export { sections };
