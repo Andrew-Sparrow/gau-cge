37ec08c8e2cee2de73fb43485b2939c011d9af07
Andrew <8911102@gmail.com>
2023-02-06 17:40:07 +0500

    adds styles for header & footer

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
+import './header.scss'
 
 function Header(props) {
   return (
-    <div>
-      <h1 className='main__title'>Chat</h1>
+    <div className='header'>
+      <h1 className='header__title'>Chat</h1>
       <a
-        className="main__link"
+        className="header__link"
         target="_blank"
         href='https://github.com/Andrew-Sparrow/gau-cge'
         alt='Link to GitHub'
--- src/components/main/main.jsx
+++ src/components/main/main.jsx
@@ -1,9 +1,20 @@
 import React from 'react';
 
+import { statements } from '../../db/statements';
+import { Statement } from '../statement/statement';
+import './main.scss'
+
 function Main(props) {
   return (
     <main className='main'>
-
+      <ul>
+        {statements.map(({statement}) =>
+          <Statement
+            key={statement.id}
+            code={statement.number}
+          />
+        )}
+      </ul>
     </main>
   );
 }
