504abd95005d1d21f0f39375a88a397fe5f2b2f8
Andrew <8911102@gmail.com>
2023-02-06 15:34:47 +0500

    adds sendDate field to messages data

--- src/db/messages.js
+++ src/db/messages.js
@@ -3,49 +3,63 @@ const messages = [
     message: {
       id: 1,
       isMy: true,
-      isUnread: false
+      isUnread: false,
+      text: "Здравствуйте",
+      sendDate: "2012-04-23T18:25:43.511Z"
     }
   },
   {
     message: {
       id: 2,
       isMy: true,
-      isUnread: false
+      isUnread: false,
+      text: "Здравствуйте",
+      sendDate: "2012-04-23T18:25:43.511Z"
     }
   },
   {
     message: {
       id: 3,
       isMy: false,
-      isUnread: true
+      isUnread: true,
+      text: "Здравствуйте",
+      sendDate: "2012-04-23T18:25:43.511Z"
     }
   },
   {
     message: {
       id: 4,
       isMy: false,
-      isUnread: true
+      isUnread: true,
+      text: "Здравствуйте",
+      sendDate: "2012-04-23T18:25:43.511Z"
     }
   },
   {
     message: {
       id: 5,
       isMy: true,
-      isUnread: false
+      isUnread: false,
+      text: "Здравствуйте",
+      sendDate: "2012-04-23T18:25:43.511Z"
     }
   },
   {
     message: {
       id: 6,
       isMy: false,
-      isUnread: true
+      isUnread: true,
+      text: "Здравствуйте",
+      sendDate: "2012-04-23T18:25:43.511Z"
     }
   },
   {
     message: {
       id: 7,
       isMy: false,
-      isUnread: true
+      isUnread: true,
+      text: "Здравствуйте",
+      sendDate: "2012-04-23T18:25:43.511Z"
     }
   }
 ];
