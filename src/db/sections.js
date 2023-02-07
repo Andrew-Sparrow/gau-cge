f219924c4cfe75508041ef52e40264a41b20fe93
Andrew <8911102@gmail.com>
2023-02-07 17:10:20 +0500

    [fix] grammatyically mistake in sections.db was fixed

--- src/db/sections.js
+++ src/db/sections.js
@@ -3,7 +3,7 @@ const sections = [
     section: {
       id: 1,
       name: 'АР - Архитектурные решения',
-      undreadCount: 0,
+      unreadCount: 0,
       messagesId: [1]
     }
   },
@@ -11,7 +11,7 @@ const sections = [
     section: {
       id: 2,
       name: 'СМ - Сметы',
-      undreadCount: 1,
+      unreadCount: 1,
       messagesId: [2]
     }
   },
@@ -19,7 +19,7 @@ const sections = [
     section: {
       id: 3,
       name: 'КР - Конструктивные решения',
-      undreadCount: 0,
+      unreadCount: 0,
       messagesId: []
     }
   },
@@ -27,7 +27,7 @@ const sections = [
     section: {
       id: 4,
       name: 'АР - Архитектурные решения',
-      undreadCount: 1,
+      unreadCount: 1,
       messagesId: [3]
     }
   },
@@ -35,7 +35,7 @@ const sections = [
     section: {
       id: 5,
       name: 'ТП - Технический паспорт БТИ',
-      undreadCount: 0,
+      unreadCount: 0,
       messagesId: []
     }
   },
@@ -43,7 +43,7 @@ const sections = [
     section: {
       id: 6,
       name: 'КП - Кадастровый паспорт на земельный участок',
-      undreadCount: 1,
+      unreadCount: 1,
       messagesId: [4]
     }
   },
@@ -51,7 +51,7 @@ const sections = [
     section: {
       id: 7,
       name: 'ПД - Правоустанавливающие и право подтверждающие документы',
-      undreadCount: 0,
+      unreadCount: 0,
       messagesId: []
     }
   },
@@ -59,7 +59,7 @@ const sections = [
     section: {
       id: 8,
       name: 'ПП - Поэтажные планы помещений',
-      undreadCount: 2,
+      unreadCount: 2,
       messagesId: [5, 6, 7]
     }
   }
